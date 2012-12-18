c8139cd4d2faefce08ed3027fbfc5b3b9769d88a
Dani Donisa <ddonisa@gnuine.com>
2012-12-18 11:27:29 +0100

    Installing ido-overrides

---
+++ personal/ido.el
@@ -0,0 +1,6 @@
+;; IDO config
+;; Display completions vertically
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched" " [Not readable]" " [Too big]" " [Confirm]")))
+
+;; Fuzzy matching
+(setq ido-enable-flex-matching t)
