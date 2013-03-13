19d915d57e15a71c9994c8411b0e66ee6a4f8a0e
Dani Donisa <ddonisa@gnuine.com>
2013-03-13 14:19:10 +0100

    Install packages needed

---
+++ packages-needed.el
@@ -0,0 +1,21 @@
+(defvar packages-needed
+  '(deft inf-ruby magit markdown-mode paredit projectile
+                  yaml-mode zenburn-theme go-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (loop for p in prelude-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (prelude-packages-installed-p)
+  ;; check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'packages-needed)
