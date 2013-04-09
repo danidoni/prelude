eefbca82e5b632f0c6633e13ea8a9656d2c3c79e
Dani Donisa <ddonisa@gnuine.com>
2013-04-09 10:58:08 +0200

    Use C-p and C-n to scroll up and down results

--- personal/ido.el
+++ personal/ido.el
@@ -2,5 +2,11 @@
 ;; Display completions vertically
 (setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]" " [No match]" " [Matched" " [Not readable]" " [Too big]" " [Confirm]")))
 
+;; Use C-p and C-n to scroll up and down
+(defun ido-define-keys ()
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
+
 ;; Fuzzy matching
 (setq ido-enable-flex-matching t)
